b7822369a0f0ce180c8257de4b47b96efbde7d9e
liaohuqiu <liaohuqiu@gmail.com>
2016-12-19 20:55:49 +0800

    add restart

--- manager.sh
+++ manager.sh
@@ -7,29 +7,31 @@ SCRIPTFILE=`basename $0`
 
 devops_prj_path="$prj_path/devops"
 
-nexus3_image=sonatype/nexus3
-nexus3_container=nexus3
+nexus_image=sonatype/nexus3
+nexus_container=nexus
 
 base_do_init=0
 source $devops_prj_path/base.sh
 
-function run_nexus3() {
-    local data_path='/opt/data/nexus3'
+function run_nexus() {
+    local data_path='/opt/data/nexus'
     ensure_dir "$data_path"
     run_cmd "chmod a+w $data_path"
-    local args=''
+
+    local args='--restart=always'
     args="$args --dns=8.8.8.8"
+    args="$args -p 11681:8081"
     args="$args -v $data_path:/nexus-data"
-    run_cmd "docker run -it $args -h $nexus3_container --name $nexus3_container $nexus3_image"
+    run_cmd "docker run -d $args -h $nexus_container --name $nexus_container $nexus_image"
 }
 
-function stop_nexus3() {
-    stop_container $nexus3_container
+function stop_nexus() {
+    stop_container $nexus_container
 }
 
-function restart_nexus3() {
-    stop_nexus3
-    run_nexus3
+function restart_nexus() {
+    stop_nexus
+    run_nexus
 }
 
 function help() {
@@ -39,14 +41,14 @@ function help() {
             
         Valid options are:
 
-            run_nexus3
-            stop_nexus3
-            restart_nexus3
+            run_nexus
+            stop_nexus
+            restart_nexus
             
             help                      show this help message and exit
 
 EOF
 }
-ALL_COMMANDS="run_nexus3 stop_nexus3 restart_nexus3"
+ALL_COMMANDS="run_nexus stop_nexus restart_nexus"
 list_contains ALL_COMMANDS "$action" || action=help
 $action "$@"
